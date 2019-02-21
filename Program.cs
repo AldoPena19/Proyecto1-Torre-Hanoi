cb3e71a8f8fbb263fce39a463808a4829ae2a3ac
AldoPena <apenap@miumg.edu.gt>
2019-02-20 18:39:41 -0600

    Definicion Constructor instancia de clase interaccion y solucion AP

--- Program.cs
+++ Program.cs
@@ -8,11 +8,27 @@ namespace funcionTorreHanoi
 {
     class Proyecto1
     {
-        static void torresHanoi(int n, int o, int d, int aux){
-                  if (n > 0){
-                torresHanoi(n-1, o, aux, d);
+        static void torresHanoi(int n, int o, int d, int aux)
+        {
+            if (n > 0)
+            {
+                torresHanoi(n - 1, o, aux, d);
                 Console.Write("nSe Mueve anillo desde Torre " + o + " Hasta Torre " + d);
-                torreshanoi(n-1, aux, d, o);
+                torreshanoi(n - 1, aux, d, o);
             }
         }
+        static void Main(string[] args)
+        {
+            int n;
+
+            Console.Write("Favor ingresar la cantidad de anillos: ");
+            n = int.Parse(Console.ReadLine());
+
+            torresHanoi(n, 1, 3, 2);
+            Console.ReadKey();
+        }
+    }
+}
+
+
 
