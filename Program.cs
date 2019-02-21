86d52cbc1858365ecba1c0972b2686f45989fcae
AldoPena <apenap@miumg.edu.gt>
2019-02-20 19:45:19 -0600

    Arreglo estetico del programa descripcion de procedimientos output

--- Program.cs
+++ Program.cs
@@ -14,16 +14,19 @@ namespace funcionTorreHanoi
             {
                 torresHanoi(n-1, o, aux, d);
                 Console.Write("\nSe Mueve anillo desde Torre " + o + " Hasta Torre " + d);
-                torreshanoi(n-1, aux, d, o);
+                torresHanoi(n-1, aux, d, o);
             }
         }
         static void Main(string[] args)
         {
             int n;
 
+            Console.Write("J U E G O - T O R R E - D E - H A N O I");
+            Console.Write("\n\nGRUPO NO.6");
             Console.Write("Favor ingresar la cantidad de anillos: ");
             n = int.Parse(Console.ReadLine());
 
+            Console.Write("\n\nSOLUCION: \n");
             torresHanoi(n, 1, 3, 2);
             Console.ReadKey();
         }
