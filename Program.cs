39ba2c7010b74a5a020920193b2a1de766ece40a
AldoPena <apenap@miumg.edu.gt>
2019-02-20 18:27:15 -0600

    Mi primer commit inicializando desarrollo del Juego AP

--- Program.cs
+++ Program.cs
@@ -9,10 +9,4 @@ namespace funcionTorreHanoi
     class Proyecto1
     {
         static void torresHanoi(int n, int o, int d, int aux){
-            if (n > 0){
-                torresHanoi(n-1, o, aux, d);
-                Console.Write("nSe Mueve anillo desde Torre " + o + " Hasta Torre " + d);
-                torreshanoi(n-1, aux, d, o);
-            }
-        }
 
